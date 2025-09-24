9bd5008d622e0a3ee1b781f4ef2369fcb0eb2f3e
ITI171 <ITI171@isi.net>
2025-09-24 12:33:14 +0300

    day 1

---
+++ Day 1/task1.py
@@ -0,0 +1,54 @@
+#	- write a program that prints hello world
+
+print("hello world")
+
+#	- application to take a number in binary form from the user, and print it as a decimal
+
+bin_num = input("Enter binary number")
+dec_num = int(bin_num,2)
+print(dec_num)
+
+"""
+- write a function that takes a number as an argument and if the number
+		divisible by 3 return "Fizz" and if it is divisible by 5 return "buzz" and if is is
+		divisible by both return "FizzBuzz"  
+"""
+
+def div_num(num):
+    if num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "buzz"
+    elif num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    else:
+        return num
+
+#- Ask the user to enter the radius of a circle print its calculated area and circumference
+
+radius = float(input("Enter the radius of the circle"))
+area = 3.14*(radius**2)
+circumference = 2 * 3.14 * radius
+
+print(area)
+print(circumference)
+
+#- Ask the user for his name then confirm that he has entered his name (not an empty string/integers). then proceed to ask him for his email and print all this data
+
+while True:
+    name = input("Enter your name: ")
+    if name.strip().isalpha():   
+        break
+    else:
+        print("Invalid name, please enter again")
+
+email = input("Enter your email: ")
+
+print(name)
+print(email)
+
+#	- Write a program that prints the number of times the substring 'iti' occurs in a string
+
+text = input("Enter a string: ")
+count = text.count("iti")
+print(count)
